2f1ed3303a4d9e2aa4bd1d3a78955e7122f76cf8
pravasthi01 <pravasthikodiripaka@gmail.com>
2025-04-22 12:30:26 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2><b>About Us</b></h2>
+      <p>Generate Random Numbers here</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Contact Us</h2>
+      <p>Email: random_num@gmail.com</p>
+      <p>Phone: 6342874635</p>
+    </div>
+  );
+}
+
+export default Contact;
